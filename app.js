bbb676416df4c7d8bdd605ae128d5b38bbbc865c
Jhoana Pool <jhoanapool99@gmail.com>
2024-07-02 17:58:50 -0600

    Portafolio actualizado

---
+++ app.js
@@ -0,0 +1,30 @@
+//Selecciones para barra de navegacion burger
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+//Cerrar el menu de burger
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((Link) => 
+    Link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
+
+//Selección de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+//Funcionalidad de desplazar hacia arriba
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
